b4ace66c390b1d49bce3015d405fc849276c1dcc
Simone-Marengo <simone.marengo@scaiconsulting.it>
2020-07-20 14:56:04 +0200

    stable on version 4

--- src/app/insert/insert.component.ts
+++ src/app/insert/insert.component.ts
@@ -6,6 +6,7 @@ import {
   FormBuilder
 } from "@angular/forms";
 import { PageDataService } from "../../services/tabs-data.service";
+import { ToastController } from "@ionic/angular";
 
 @Component({
   selector: "app-insert",
@@ -17,16 +18,17 @@ export class InsertComponent {
   form: FormGroup;
   constructor(
     private pageDataService: PageDataService,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    public toastController: ToastController
   ) {
     this.form = formBuilder.group({
       title: [
         "",
-        [Validators.minLength(3), Validators.maxLength(10), Validators.required]
+        [Validators.minLength(3), Validators.maxLength(25), Validators.required]
       ],
       description: [
         "",
-        [Validators.minLength(3), Validators.maxLength(30), Validators.required]
+        [Validators.minLength(3), Validators.maxLength(50), Validators.required]
       ],
       label: ["", Validators.required],
       startDate: ["", Validators.required],
@@ -35,24 +37,35 @@ export class InsertComponent {
   }
 
   saveLista() {
+    debugger
     const listValue: any = this.getListValue();
-    this.form.reset();
     this.pageDataService.addElement(listValue);
+    this.presentToast();
+    this.form.reset();
   }
 
   getListValue(): any {
     const titleValue = this.form.get("title").value;
     const descriptionValue = this.form.get("description").value;
-    const labelsValue = this.form.get("labels").value;
+    const labelValue = this.form.get("label").value;
     const startDateValue = this.form.get("startDate").value;
     const endDateValue = this.form.get("endDate").value;
     const lista: any = {
       title: titleValue,
       description: descriptionValue,
-      labels: labelsValue,
+      labels: labelValue,
       startDate: startDateValue,
       endDate: endDateValue
     };
     return lista;
+  }
+
+  async presentToast() {
+    const toast = await this.toastController.create({
+      header: "Lista Salvata con Successo!",
+      position: "bottom",
+      duration: 2000
+    });
+    toast.present();
   }
 }
